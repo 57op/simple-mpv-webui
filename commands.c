23e72a4d3608455e417dc0f8096f16bc7741bf0a
chuck- <chuck-@users.noreply.github.com>
2020-05-06 15:58:07 +0200

    introduce a simple whitelist check

--- commands.c
+++ commands.c
@@ -7,17 +7,70 @@
 
 #include <libwebsockets.h>
 
+// whitelist: hashmap section
+// hashmap are generated with utils/whitelist.py
+#define BUCKET_NO 7
+#define PROPERTIES_BUCKET_LEN 5
+#define COMMANDS_BUCKET_LEN 6
+
+static const char *properties_hashmap[BUCKET_NO][PROPERTIES_BUCKET_LEN] = {
+  { "chapters", "duration", "playtime-remaining", "loop-file", NULL },
+  { "fullscreen", NULL, NULL, NULL, NULL },
+  { "sub-delay", NULL, NULL, NULL, NULL },
+  { "metadata", "track-list", NULL, NULL, NULL },
+  { "time-pos", NULL, NULL, NULL, NULL },
+  { "playlist", "chapter", "pause", "volume", "volume-max" },
+  { "filename", "audio-delay", "loop-playlist", NULL, NULL }
+};
+
+static const char *commands_hashmap[BUCKET_NO][COMMANDS_BUCKET_LEN] = {
+  { "seek 10", "add chapter 1", "cycle audio", "add sub-scale -0.1", NULL, NULL },
+  { "set loop-playlist", "playlist-remove", "set volume", "sub_seek 1", "add sub-delay -0.05", "cycle fullscreen" },
+  { "set loop-file", "seek -10", "playlist-next", "cycle sub", "add sub-scale 0.1", "cycle audio-device" },
+  { "playlist-shuffle", NULL, NULL, NULL, NULL, NULL },
+  { "cycle pause", "cycle pause", "playlist-move", "cycle pause", "add sub-delay 0.05", NULL },
+  { "add chapter -1", NULL, NULL, NULL, NULL, NULL },
+  { "set playlist-pos", "seek absolute", "sub_seek -1", "add audio-delay -0.05", "add audio-delay 0.05", NULL }
+};
+
+static size_t hash_k33(const char *str) {
+  size_t hash = 5381;
+  char c;
+
+  while ((c = *str++) != '\0') {
+    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+  }
+
+  return hash % BUCKET_NO;
+}
+// /whitelist: hashmap section
+
 // used for returning json
 #define RET_MAX_SIZE 0x2000
 static char ret_val[LWS_SEND_BUFFER_PRE_PADDING + RET_MAX_SIZE + LWS_SEND_BUFFER_POST_PADDING];
 
-static char *command_get_property(mpv_handle *mpv, const char *prop) {
-  char *value = mpv_get_property_string(mpv, prop);
+static char *command_get_property(mpv_handle *mpv, const char *property) {
+  // check if property is in whitelisted properties (hashmap)
+  const char **bucket = properties_hashmap[hash_k33(property)];
+  uint8_t found = 0;
+
+  for (size_t i = 0; i < PROPERTIES_BUCKET_LEN && bucket[i] != NULL && found == 0; i++) {
+    found = strcmp(property, bucket[i]) == 0;
+  }
+
+  char *value = NULL;
   char *result = &ret_val[LWS_SEND_BUFFER_PRE_PADDING];
 
-  if (value == NULL) {
-    value = mpv_get_property_osd_string(mpv, prop);
-  }
+  if (found == 1) {
+    value = mpv_get_property_string(mpv, property);
+
+
+    if (value == NULL) {
+      value = mpv_get_property_osd_string(mpv, property);
+    }
+  }/* else {
+    fprintf(stderr, "[command_get_property] unkown property: %s\n", property);
+  }*/
 
   if (value == NULL) {
     strcpy(result, "false");
@@ -25,9 +78,9 @@ static char *command_get_property(mpv_handle *mpv, const char *prop) {
     // some properties return a json
     // we won't wrap json with quotes
     const uint8_t is_json =
-      strcmp(prop, "metadata") == 0 ||
-      strcmp(prop, "playlist") == 0 ||
-      strcmp(prop, "track-list") == 0;
+      strcmp(property, "metadata") == 0 ||
+      strcmp(property, "playlist") == 0 ||
+      strcmp(property, "track-list") == 0;
 
     snprintf(result, RET_MAX_SIZE, is_json ? "%s" : "\"%s\"", value);
     mpv_free(value);
@@ -36,8 +89,23 @@ static char *command_get_property(mpv_handle *mpv, const char *prop) {
   return ret_val;
 }
 
-static char *command_command(mpv_handle *mpv, const char *cmd) {
-  int status = mpv_command_string(mpv, cmd);
+static char *command_command(mpv_handle *mpv, const char *command) {
+  // check if command is in whitelisted commands (hashmap)
+  const char **bucket = commands_hashmap[hash_k33(command)];
+  uint8_t found = 0;
+
+  for (size_t i = 0; i < COMMANDS_BUCKET_LEN && bucket[i] != NULL && found == 0; i++) {
+    // commands starts with bucket[i]
+    found = strncmp(bucket[i], command, strlen(bucket[i])) == 0;
+  }
+
+  int status = -1;
+
+  if (found == 1) {
+    status = mpv_command_string(mpv, command);
+  } else {
+    fprintf(stderr, "[command_command] unkown command: %s\n", command);
+  }
 
   strcpy(&ret_val[LWS_SEND_BUFFER_PRE_PADDING], status == 0 ? "true" : "false");
 
---
+++ utilities/whitelist.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import subprocess
+import re
+
+from pathlib import Path
+from typing import List, Tuple
+
+REGEX_PROP_CMD = re.compile(r'(command|get_property)\s*\(\s*([\'`])(.+?)\2\s*\)')
+
+def hash_k33(s: str, b: int) -> int:
+  # invoking a c compiled program because i was having some mismatches
+  # i will inspect this later on, not a priority
+  p = subprocess.Popen(('utilities/hash', str(b), s), stdout=subprocess.PIPE)
+  return int(p.stdout.read().decode().strip())
+
+def find_props_cmds(file: Path) -> List[Tuple[str, str]]:
+  matches = []
+
+  with open(file, 'r') as fh:
+    for line in fh:
+      for m in REGEX_PROP_CMD.finditer(line):
+        matches.append((m.group(1), m.group(3)))
+
+  return matches
+
+def to_c_array(hashmap: List[List[str]], name: str) -> str:
+  max_bucket_len = max(map(lambda bucket: len(bucket), hashmap))
+  c = []
+
+  for bucket in hashmap:
+    bucket = list(map(lambda key: f'"{key}"', bucket))
+
+    if len(bucket) < max_bucket_len:
+      bucket.extend(['NULL'] * (max_bucket_len - len(bucket)))
+
+    c.append(f'  {{ {", ".join(bucket)} }}')
+
+  c = ',\n'.join(c)
+
+  return f'const char *{name}[{len(hashmap)}][{max_bucket_len}] = {{\n{c}\n}};'
+
+if __name__ == '__main__':
+  import argparse
+
+  def ext_path(p):
+    p = Path(p)
+
+    if not p.is_file():
+      raise argparse.ArgumentTypeError(f'no such file `{p}`')
+
+    return p
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('files', nargs='+', type=ext_path)
+  parser.add_argument('--buckets', type=int, default=5)
+
+  args = parser.parse_args()
+  props = []
+  cmds = []
+
+  for file in args.files:
+    for (fn, arg) in find_props_cmds(file):
+      if fn == 'get_property':
+        props.append(arg)
+      else:
+        arg = ' '.join(re.sub('\$\{.+?\}', '', arg).split())
+        cmds.append(arg)
+
+  max_prop_len = max(map(lambda prop: len(prop), props))
+  max_cmd_len = max(map(lambda cmd: len(cmd), cmds))
+
+  # build a static hashmap for properties
+  buckets = args.buckets
+  hashmap = [[] for i in range(buckets)]
+
+  for prop in props:
+    hashmap[hash_k33(prop, buckets)].append(prop)
+
+  print(to_c_array(hashmap, 'props_hashmap'))
+
+  # build a static hashmap for commands
+  hashmap = [[] for i in range(buckets)]
+
+  for cmd in cmds:
+    hashmap[hash_k33(cmd, buckets)].append(cmd)
+
+  print(to_c_array(hashmap, 'cmds_hashmap'))
